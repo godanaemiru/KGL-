d265b8664d1e17dd9f2ed9c3e4308f148b4b870f
Godana Emiru <godanaemiru@gmail.com>
2026-03-11 02:02:35 +0300

    updated backend url

--- karibu-frontend/src/services/api.js
+++ karibu-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://kgl-ogni.onrender.com/api',
 });
 
 // Automatically add the token to the header if it exists in local storage
